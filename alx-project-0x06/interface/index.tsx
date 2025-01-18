e391d1c23308de47a0a3edc0d64fe489bf110876
kamaufnjeri <kamaufnjeri2019@gmail.com>
2025-01-18 15:36:14 +0300

    state management task 2

---
+++ alx-project-0x06/interface/index.tsx
@@ -0,0 +1,18 @@
+import * as React from "react"
+
+
+export interface ButtonProps {
+    buttonLabel: string;
+    buttonSize?: string;
+    buttonBackgroundColor?: "red" | "blue" | "orange" | "green";
+    action?: () => void;
+}
+
+export interface LayoutProps {
+    children: React.ReactNode;
+}
+
+export interface PageRouteProps {
+    pageRoute: string
+}
+  
